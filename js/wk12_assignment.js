1c1aacb67c91e02a78a47da273e90517c95f8235
GWelk <gena.welk@colorado.edu>
2022-04-09 23:21:29 -0600

    Assignment 12

--- js/wk12_assignment.js
+++ js/wk12_assignment.js
@@ -1,3 +1,4 @@
+
 //These all do the same thing, but the best choice is the third or 4th line
 // because they are the most specific.
 // var quoteButton = document.queryselector('button');
@@ -5,36 +6,75 @@
 // var quoteButton = document.getelementbyid('js-new-quote');
 // var quoteButton = document.queryselector('#js-new-quote');
 // let quoteButton = document.queryselector('#js-new-quote');
+var whosaidit = null;
 const quoteButton = document.querySelector('#js-new-quote');
+const whoButton = document.querySelector('#who');
 
 //What is the difference between queryselector or getelementbyid
 //What is the difference between let, var, and const
 
 quoteButton.addEventListener('click',getQuote);
+whoButton.addEventListener('click',printCharacter);
 
+//This function is asynchronous.
+//This means means we can use the 'await' keyword below it.
+//Await will pause the funtion while waiting for an operation to complete.
+//More info here:  https://javascript.info/async-await
 async function getQuote () {
+  //This line isn't necessary, but is a debug tool.
+  //We can see what's happening by viewing the log.
   console.log("quote button was clicked");
-  try {
-    const response = await fetch(endpoint);
-    if (!response.ok){
-      throw Error(response.statusText);
-    }
 
+  // The `try` block executes the statements within.
+  // If an exception is thrown (if the variable 'response' is not ok,
+  //it will throw an error - I am not exactly sure what response.statusText is),
+  //the statements defined in
+  // the `catch` block will be executed.
+  // More info is here: https://javascript.info/try-catch
+  try {
+    const response = await fetch(endpoint,
+      // {credentials: 'include'}
+    );
+    // If the response is not 200 OK...
+      if (!response.ok){
+        // ...throw an error. This causes control flow
+        // to skip to the `catch` block below.
+        throw Error(response.statusText);
+      }
     const json = await response.json();
     console.log(json);
-    displayQuote(json.fact);
+    displayQuote(json.quote);
+    var whosaidit = (json.character);
+    console.log(whosaidit);
+    // printCharacter(whosaidit);
   } catch(err) {
-    console.log(err);
-    alert('Failed');
+      console.log(err);
+      alert('Failed');
+    }
   }
-}
+
+
+
+// function displayQuote(quote) {
+//   const quoteText = document.querySelector('#js-quote-text');
+//   quoteText.textContent = quote;
+// }
 
 function displayQuote(quote) {
-  const quoteText = document.querySelector('#js-quote-text');
-  quoteText.textContent = quote;
+  document.querySelector('#js-quote-text').textContent = quote;
+}
+
+function printCharacter(whosaidit) {
+  document.querySelector('#whichFriend').textContent = whosaidit;
 }
 
 //const endpoint ='https://random-quote-generator.herokuapp.com/api/quotes/random'
 //const endpoint = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1'
 //const endpoint = 'http://quotes.stormconsultancy.co.uk/random.json'
-const endpoint = 'https://catfact.ninja/fact'
+// const endpoint = 'https://catfact.ninja/fact';
+//use displayQuote(json.fact)
+// const endpoint = 'https://randommarvelquoteapi.herokuapp.com/';
+//use displayQuote(json.quote)
+// const endpoint = 'https://zenquotes.io/api/quotes/';
+const endpoint = 'https://friends-quotes-api.herokuapp.com/quotes/random';
+// displayQuote(json.quote);
