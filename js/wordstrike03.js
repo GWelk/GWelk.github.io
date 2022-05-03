511658f6f52265ce97ef4260217c6dccc75d193a
GWelk <gena.welk@colorado.edu>
2022-05-02 20:50:06 -0600

    tried to add eventListener to letters
    It didn't work because all the letters weren't invented yet.

--- js/wordstrike03.js
+++ js/wordstrike03.js
@@ -100,8 +100,9 @@ function printStartingAlpha() {
   for (const x of startingAlphabet) {
     availableLetters = "<li class='simple' id='" + x+ "'> " + x+ " </li>";
     letterUL.innerHTML += availableLetters;
+    document.querySelector('#a').addEventListener('click',callLetter);
+    document.querySelector('#b').addEventListener('click',callLetter);
   }
-  document.querySelector('#a').addEventListener('click',callLetter);
 }
 
 function callLetter (axc) {
